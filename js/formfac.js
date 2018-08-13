e38c8df0cb79fd271b0902f83644375836765d58
shreyaskargit <shreyaskar30@gmail.com>
2018-08-13 23:43:42 +0530

    A few Updates

--- js/formfac.js
+++ js/formfac.js
@@ -6,8 +6,10 @@
 // 		eve[i].style.borderBottomColor = "rgb(94, 155, 255)";
 // 	});
 // }
-$("#lform input").click(function(event){
-	$(this).css("border-bottom-color", "rgb(94, 155, 255)");
+$(".lform input").click(function(event){
+	// $(this).css("border-bottom-color", "red");
+	// $(this).css("border-bottom", "4px solid red");
+	// console.log("clicked");
 });
 $("#submit").click(function(event){
 	window.location = './teacher.html'
